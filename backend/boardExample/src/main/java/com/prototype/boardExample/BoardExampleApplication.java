91a71f88c99aa26ff1c7872dd27769afb35d0fcd
SeungkeyMouse <ksg19980@gmail.com>
2022-04-28 01:13:49 +0900

    commit

---
+++ backend/boardExample/src/main/java/com/prototype/boardExample/BoardExampleApplication.java
@@ -0,0 +1,13 @@
+package com.prototype.boardExample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardExampleApplication {
+//https://4coops.tistory.com/entry/React-Spring-Boot-CRUD-%EB%A7%8C%EB%93%A4%EA%B8%B0
+	public static void main(String[] args) {
+		SpringApplication.run(BoardExampleApplication.class, args);
+	}
+
+}
---
+++ backend/boardExample/src/test/java/com/prototype/boardExample/BoardExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prototype.boardExample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
